cf7d5e7a4a50594cb8f800f7ddcdd06132c75870
MilcahHalili <halilicenteno@gmail.com>
2018-10-22 15:17:02 -0700

    add mobile responsiveness to redesign

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
               </div>
             </header>
             <div>
-                <h3><strong className="App-caps"><a id="summary">SUMMARY</a></strong></h3>
+              <h3><strong className="App-caps"><a id="summary">SUMMARY</a></strong></h3>
               <div className="summary-div">
                 <p>
                   Hi, my name is Milcah. I'm a full stack web developer building with <strong>M</strong>ongoDB, <strong>E</strong>xpress, <strong>R</strong>eactJS, <strong>N</strong>ode.js stack and more.
@@ -56,121 +56,123 @@ class App extends Component {
                 </p>
               </div>
             </div>
-            <div>
-                <h3><strong className="App-caps"><a id="skills">SKILLS</a></strong></h3>
-
+            <div className="skills-div">
+              <h3><strong className="App-caps"><a id="skills">SKILLS</a></strong></h3>
               <table>
-                <tr>
-                  <th>Languages</th>
-                  <th>Libraries</th>
-                  <th>Databases</th>
-                  <th>Frameworks</th>
-                  <th>Tools</th>
-                </tr>
-                <tr>
-                  <td>HTML</td>
-                  <td>React</td>
-                  <td>MongoDB</td>
-                  <td>Express</td>
-                  <td>AWS</td>
-                </tr>
-                <tr>
-                  <td>CSS</td>
-                  <td>jQuery</td>
-                  <td>PostgreSQL</td>
-                  <td>Ruby on Rails</td>
-                  <td>Slack</td>
-                </tr>
-                <tr>
-                  <td>JavaScript</td>
-                  <td>Bootstrap</td>
-                  <td></td>
-                  <td>Socket.IO</td>
-                  <td>GitHub</td>
-                </tr>
-                <tr>
-                  <td>Ruby</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td>Heroku</td>
-                </tr>
-                <tr>
-                  <td>PHP</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
+                <tbody>
+                  <tr>
+                    <th>Languages</th>
+                    <th>Libraries</th>
+                    <th>Databases</th>
+                    <th>Frameworks</th>
+                    <th>Tools</th>
+                  </tr>
+                  <tr>
+                    <td>HTML</td>
+                    <td>React</td>
+                    <td>MongoDB</td>
+                    <td>Express</td>
+                    <td>AWS</td>
+                  </tr>
+                  <tr>
+                    <td>CSS</td>
+                    <td>jQuery</td>
+                    <td>PostgreSQL</td>
+                    <td>Ruby on Rails</td>
+                    <td>Slack</td>
+                  </tr>
+                  <tr>
+                    <td>JavaScript</td>
+                    <td>Bootstrap</td>
+                    <td></td>
+                    <td>Socket.IO</td>
+                    <td>GitHub</td>
+                  </tr>
+                  <tr>
+                    <td>Ruby</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>Heroku</td>
+                  </tr>
+                  <tr>
+                    <td>PHP</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
               </table>
-
             </div>
             <div>
               <h3><strong className="App-caps"><a id="projects">PROJECTS</a></strong></h3>
 
               <table>
-                <tr>
-                  <td>
-                    <div className="App-projects-div">
-                      <h4>WeVote.US</h4>
-                      <a href="https://wevote.us/ballot">
-                        <img alt="WeVote.US" className="App-projects-img" src="https://i.imgur.com/f7fOR0p.png" />
-                      </a>
-                    </div>
-                  </td>
-                  <td>
-                    <div className="App-projects-div">
-                      <h4>game.it</h4>
-                      <a href="https://game-it.herokuapp.com/">
-                        <img alt="game.it-app" className="App-projects-img" src="https://i.imgur.com/0QPgqk5.png" />
-                      </a>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="App-projects-div">
+                        <a href="https://wevote.us/ballot"><h4>WeVote.US</h4></a>
+                        <a href="https://wevote.us/ballot">
+                          <img alt="WeVote.US" className="App-projects-img" src="https://i.imgur.com/f7fOR0p.png" />
+                        </a>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="App-projects-div">
+                        <a href="https://game-it.herokuapp.com/"><h4>game.it</h4></a>
+                        <a href="https://game-it.herokuapp.com/">
+                          <img alt="game.it-app" className="App-projects-img" src="https://i.imgur.com/0QPgqk5.png" />
+                        </a>
 
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <div className="App-projects-div">
-                    <h4>Stock Pot Images</h4>
-                      <a href="https://www.stockpotimages.com/">
-                        <img alt="Stock-Pot-Images-app" className="App-projects-img" src="https://i.imgur.com/RNdJZ97.png" />
-                      </a>
-                    </div>
-                  </td>
-                  <td>
-                    <div className="App-projects-div">
-                      <h4>Simon</h4>
-                      <a href="https://milcahhalili.github.io/Simon/">
-                        <img alt="Simon-app" className="App-projects-img" src="https://i.imgur.com/rafNHY9.png" />
-                      </a>
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <div className="App-projects-div">
-                      <h4>Burst Book</h4>
-                      <a href="https://burstbook.herokuapp.com/">
-                        <img alt="Burst-Book-app" className="App-projects-img" src="https://i.imgur.com/lHMBBQa.png" />
-                      </a>
-                    </div>
-                  </td>
-                  <td>
-                    <div className="App-projects-div">
-                      <h4>flwr</h4>
-                      <a href="https://flwr-app.herokuapp.com/">
-                        <img alt="flwr-app" className="App-projects-img" src="https://i.imgur.com/vPLWmio.png" />
-                      </a>
-                    </div>
-                  </td>
-                </tr>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div className="App-projects-div">
+                        <a href="https://www.stockpotimages.com/"><h4>Stock Pot Images</h4></a>
+                        <a href="https://www.stockpotimages.com/">
+                          <img alt="Stock-Pot-Images-app" className="App-projects-img" src="https://i.imgur.com/RNdJZ97.png" />
+                        </a>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="App-projects-div">
+                        <a href="https://milcahhalili.github.io/Simon/"><h4>Simon</h4></a>
+                        <a href="https://milcahhalili.github.io/Simon/">
+                          <img alt="Simon-app" className="App-projects-img" src="https://i.imgur.com/rafNHY9.png" />
+                        </a>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div className="App-projects-div">
+                        <a href="https://burstbook.herokuapp.com/"><h4>Burst Book</h4></a>
+                        <a href="https://burstbook.herokuapp.com/">
+                          <img alt="Burst-Book-app" className="App-projects-img" src="https://i.imgur.com/lHMBBQa.png" />
+                        </a>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="App-projects-div">
+                        <a href="https://flwr-app.herokuapp.com/"><h4>flwr</h4></a>
+                        <a href="https://flwr-app.herokuapp.com/">
+                          <img alt="flwr-app" className="App-projects-img" src="https://i.imgur.com/vPLWmio.png" />
+                        </a>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
               </table>
 
             </div>
             <div className="contact-div">
-                <h3><strong className="App-caps"><a id="contact">CONTACT</a></strong></h3>
-                <p>halilicenteno@gmail.com</p>
-                <p>(510) 944-3701</p>
+              <h3><strong className="App-caps"><a id="contact">CONTACT</a></strong></h3>
+              <p>halilicenteno@gmail.com</p>
+              <p>(510) 944-3701</p>
             </div>
           </div>
         </div>
